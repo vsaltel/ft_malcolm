1e8ef4f60decd0a63038b67a867ad4132d0192f5
Valentin <saltel.valentin@gmail.com>
2021-05-12 21:57:33 +0200

    send

--- srcs/send.c
+++ srcs/send.c
@@ -46,7 +46,7 @@ void	send_arp(t_malcolm *mal, char *recvbuf)
 	struct sockaddr lala;
 	struct sockaddr_in *toto;
 	toto = (struct sockaddr_in *)&lala;
-	toto->sin_addr.s_addr = *(uint32_t *)bef->sender_ip;
+	toto->sin_addr.s_addr = *(uint32_t *)arp->target_ip;
 	toto->sin_family = AF_INET;
 
 	ret = sendto(mal->sockfd, buf, sizeof(t_arp), 0, &lala, sizeof(lala));
