74d0a6844e030aa016eb173d620bd64379b078a3
Valentin <saltel.valentin@gmail.com>
2021-05-17 15:41:11 +0200

    includes

--- includes/malcolm.h
+++ includes/malcolm.h
@@ -17,8 +17,8 @@
 # include <sys/types.h>
 # include <sys/socket.h>
 # include <arpa/inet.h>
-# include <netinet/in_systm.h>
-# include <netinet/in.h>
+//# include <netinet/in_systm.h>
+//# include <netinet/in.h>
 # include <netinet/ip.h>
 # include <ifaddrs.h>
 # include <netinet/if_ether.h>
