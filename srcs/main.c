8ff9f790e5ed1340e2206f8f123aac2172bd6f8f
Valentin <saltel.valentin@gmail.com>
2021-05-17 22:43:05 +0200

    fix

--- srcs/main.c
+++ srcs/main.c
@@ -14,7 +14,10 @@ int	main(int argc, char **argv)
 	init_malcolm(&g_mal);
 	signal(SIGINT, &catch_sigint);
 	if (get_args(&g_mal, argc, argv))
+	{
+		free_args(&g_mal);
 		return (1);
+	}
 	ret = malcolm(&g_mal);
 	printf("Exiting program...\n");
 	free_args(&g_mal);
